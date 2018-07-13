bfc8c1ecbb2e62184d355a9498bf6ec37da93eaf
cjinqing <1194885211@qq.com>
2018-07-14 00:17:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myduty/MyFragment1.java
@@ -0,0 +1,19 @@
+package com.example.administrator.myduty;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created on 2018/6/28.
+ */
+public class MyFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/myduty/MyFragment3.java
@@ -0,0 +1,24 @@
+package com.example.administrator.myduty;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created on 2018/6/28.
+ */
+public class MyFragment3 extends Fragment {
+
+    public MyFragment3() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content2,container,false);
+        return view;
+    }
+}
